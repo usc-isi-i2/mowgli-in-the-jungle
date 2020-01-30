8ad33a1a18d378b15355a7c42e095b38bf8fdbe6
Filip Ilievski <ilievski@ckg01.isi.edu>
2020-01-29 16:31:37 -0800

    gt test pat

--- tests/gt.py
+++ tests/gt.py
@@ -4,9 +4,11 @@ import utils.general as utils
 
 import utils.gt.io_utils as gtio
 
-mowgli_nodes='/Users/filipilievski/mowgli-dev-framework/kb/nodes_v002.csv'
-mowgli_edges='/Users/filipilievski/mowgli-dev-framework/kb/edges_v002.csv'
-output_gml='/Users/filipilievski/mowgli-dev-framework/kb/graph.graphml'
+#datadir='~/mowgli-in-the-jungle/kb/'
+datadir='/nas/home/ilievski/mowgli-in-the-jungle/kb/'
+mowgli_nodes=f'{datadir}nodes_v002.csv'
+mowgli_edges=f'{datadir}edges_v002.csv'
+output_gml=f'{datadir}graph.graphml'
 
 gtio.transform_to_graphtool_format(mowgli_nodes, mowgli_edges, output_gml, True)
 g=gtio.load_graph(output_gml)
