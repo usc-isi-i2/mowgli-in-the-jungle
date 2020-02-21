bb29d18717100b5322829b7a883285dcccbdef32
Filip Ilievski <filip.dbrsk@gmail.com>
2020-02-21 10:27:16 -0800

    updated install script

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+conda create -n mowgli-env python=3.6 anaconda
+source activate mowgli-env
+
 git clone https://github.com/ucinlp/mowgli-uci.git
 cd mowgli-uci
 pip install -r requirements.txt
@@ -7,3 +10,6 @@ python -m spacy download en_core_web_lg
 curl https://conceptnet.s3.amazonaws.com/downloads/2019/numberbatch/numberbatch-en-19.08.txt.gz --output numberbatch-en-19.08.txt.gz
 gunzip numberbatch-en-19.08.txt.gz
 cd ..
+
+pip install -r requirements.txt
+conda install -c conda-forge graph-tool
