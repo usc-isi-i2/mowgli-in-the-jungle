4b3b0bf104b384cfc05c3c56960c4ac1a4cffca5
Filip Ilievski <filip.dbrsk@gmail.com>
2020-02-21 10:24:17 -0800

    mowgli predictor - implemented grounding

--- utils/grounding/uci_utils.py
+++ utils/grounding/uci_utils.py
@@ -6,6 +6,15 @@ sys.path.append('mowgli-uci')
 import graphify
 import link
 
+def get_concepts(nodes, normalize_nodes):
+	concepts=[]
+	for node_id, node_data in nodes.items():
+		best_candidate=node_data['candidates'][-1]["uri"]
+		if normalize_nodes:
+			best_candidate=best_candidate.lstrip('/c/en/')
+		concepts.append(best_candidate)
+	return concepts
+
 def generate_instances(l):
 	for graph in l:
 		yield graph
