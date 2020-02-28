18992071dcca8eac56c52e7cb5bb09cf26e4a721
Filip Ilievski <6437407+filievski@users.noreply.github.com>
2020-02-28 15:56:10 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,27 @@
-from setuptools import setup, find_packages
+from distutils.core import setup
+from setuptools import find_packages
 
+with open('requirements.txt', 'r') as f:
+    install_requires = list()
+    dependency_links = list()
+    for line in f:
+        re = line.strip()
+        if re:
+            if re.startswith('git+') or re.startswith('svn+') or re.startswith('hg+'):
+                dependency_links.append(re)
+            else:
+                install_requires.append(re)
+
+packages = find_packages()
 
 setup(
-    name="mowgli_framework",
+    name='mowgli_framework',
     version="0.1.0",
-    packages=find_packages(),
-    install_requires=[
-
-    ]
+    packages=packages,
+    url='https://github.com/usc-isi-i2/mowgli-in-the-jungle',
+    license='MIT',
+    author='Filip Ilievski',
+    include_package_data=True,
+    install_requires=install_requires,
+    dependency_links=dependency_links
 )
