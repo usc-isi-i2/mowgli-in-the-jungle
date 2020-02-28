f7a3875f37f5668209b520c5fb763f6b7586c1e5
Filip Ilievski <6437407+filievski@users.noreply.github.com>
2020-02-28 14:30:19 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,11 @@
-import setuptools
+from setuptools import setup, find_packages
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
 
-with open('requirements.txt', 'r') as f:
-    install_requires = list()
-    for line in f:
-        re = line.strip()
-        if re:
-            install_requires.append(re)
-
-setuptools.setup(
+setup(
     name="mowgli_framework",
-    version="1.0.0",
-    author="Filip Ilievski",
-    author_email="ilievski@isi.edu",
-    description="Commonsense framework",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/usc-isi-i2/mowgli-in-the-jungle",
-    packages=setuptools.find_packages(),
-    classifiers=(
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent"
-    ),
-    install_requires=install_requires
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+
+    ]
 )
