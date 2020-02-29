52e732b381587a689c1f767979a037e45f40a6dd
Filip Ilievski <filip.dbrsk@gmail.com>
2020-02-28 16:18:39 -0800

    packaging

---
+++ mowgli/parser_config.py
@@ -0,0 +1,44 @@
+# General
+data_dir="../data"
+
+parts=['train', 'dev']
+cfg={}
+
+# Hellaswag
+cfg['hellaswag'] = {
+            "dataname": "hellaswag",
+            "train_input_file": "train.jsonl",
+            "train_labels_file": "train-labels.lst",
+            "dev_input_file": "valid.jsonl",
+            "dev_labels_file": "valid-labels.lst",
+            "answer_offset": 0
+            }
+
+# PhysicalIQA
+cfg['physicaliqa'] = {
+            "dataname": "physicaliqa",
+            "train_input_file": "train.jsonl",
+            "train_labels_file": "train-labels.lst",
+            "dev_input_file": "dev.jsonl",
+            "dev_labels_file": "dev-labels.lst",
+            "answer_offset": 0
+            }
+
+# SocialIQA
+cfg['socialiqa'] = {
+            "dataname": "socialiqa",
+            "train_input_file": "train.jsonl",
+            "train_labels_file": "train-labels.lst",
+            "dev_input_file": "dev.jsonl",
+            "dev_labels_file": "dev-labels.lst",
+            "answer_offset": 1
+            }
+
+cfg['anli'] = {
+            "dataname": "anli",
+            "train_input_file": "train.jsonl",
+            "train_labels_file": "train-labels.lst",
+            "dev_input_file": "dev.jsonl",
+            "dev_labels_file": "dev-labels.lst",
+            "answer_offset": 1
+        }
---
+++ mowgli/predictor/example_predictor/example_predictor.py
@@ -0,0 +1,24 @@
+import random
+from typing import List, Any
+
+from classes import Dataset, Entry
+from predictor.predictor import Predictor
+
+class ExamplePredictor(Predictor):
+
+    def preprocess(self, part_data:List, partition:str) -> Any:
+        return part_data
+        
+    def train(self, train_data:List, dev_data: List) -> Any:
+        return None
+
+    def predict(self, model: Any, entry: Entry) -> List:
+        question=entry.question
+        answers=entry.answers
+
+        answer=random.randint(0,len(answers)-1)
+        while answers[answer]=='':
+            answer=random.randint(0,len(answers)-1)
+        probs=['%.2f' % random.random() for i in range(len(answers))]
+        return str(answer), probs
+
