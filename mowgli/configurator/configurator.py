77c5923d4ff973cbea250f2e3b7fac478f3c0660
Filip Ilievski <6437407+filievski@users.noreply.github.com>
2020-03-26 08:52:37 -0700

    Update configurator.py

--- mowgli/configurator/configurator.py
+++ mowgli/configurator/configurator.py
@@ -22,8 +22,8 @@ class Configurator(object):
             for keyword in self.args[component_key]:
                 if keyword != "class":
                     kwargs[keyword] = self.args[component_key][keyword]
-            print(kwargs)
-            return provider(**kwargs)
+            return provider()
+            #return provider(**kwargs)
 
     def get_component(self, component_key):
         return self.get_instance(component_key)
