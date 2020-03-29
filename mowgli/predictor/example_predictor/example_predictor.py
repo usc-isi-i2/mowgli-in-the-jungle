af222a1d20d0d97d21475f74511bbe92734ad125
Filip Ilievski <6437407+filievski@users.noreply.github.com>
2020-03-28 21:52:58 -0700

    Update example_predictor.py

--- mowgli/predictor/example_predictor/example_predictor.py
+++ mowgli/predictor/example_predictor/example_predictor.py
@@ -12,13 +12,18 @@ class ExamplePredictor(Predictor):
     def train(self, train_data:List, dev_data: List, graph: Any) -> Any:
         return None
 
-    def predict(self, model: Any, entry: Entry) -> List:
-        question=entry.question
-        answers=entry.answers
+    def predict(self, model: Any, dataset: Dataset, partition: str) -> List:
+        entries=getattr(dataset, partition)
+        all_answers= []
+        all_probs = []
+        for entry in entries:
+            question=entry.question
+            answers=entry.answers
 
-        answer=random.randint(0,len(answers)-1)
-        while answers[answer]=='':
             answer=random.randint(0,len(answers)-1)
-        probs=['%.2f' % random.random() for i in range(len(answers))]
-        return str(answer), probs
-
+            all_answers.append(answer)
+            while answers[answer]=='':
+                answer=random.randint(0,len(answers)-1)
+            probs=['%.2f' % random.random() for i in range(len(answers))]
+            all_probs.append(probs)
+        return all_answers, all_probs
