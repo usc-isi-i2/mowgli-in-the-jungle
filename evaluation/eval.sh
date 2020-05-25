0cb467cddbd82cb19a1d7db5ffad13e15abb4508
Filip Ilievski <filip.dbrsk@gmail.com>
2020-05-25 10:18:06 -0700

    docker and evaluation removed

--- evaluation/eval.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-dataname='hellaswag'
-part='train'
-
-labels_file="raw/hellaswag-train-dev/${part}-labels.lst"
-preds_file="output/${dataname}/${part}.lst"
-
-python eval.py --labels_file $labels_file --preds_file $preds_file 
