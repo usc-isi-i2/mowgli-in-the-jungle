7808fec7880e61e7999706f10b58c44a2db4bd68
ehsan <qasemi.ehs@gmail.com>
2020-05-25 18:32:33 -0700

    updates:   - clean up the branch

--- evaluation/eval.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-dataname='hellaswag'
-part='tune'
-
-labels_file="raw/hellaswag-tune-dev/${part}-labels.lst"
-preds_file="output/${dataname}/${part}.lst"
-
-python eval.py --labels_file $labels_file --preds_file $preds_file 
